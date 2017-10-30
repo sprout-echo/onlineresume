681fa4455f80825cdcafce0407b6ca4c801043c5
小YI酱 <775249762@qq.com>
2017-10-30 12:26:35 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+import store from './store/index'
+
+export default {
+    name: 'app',
+    store
+}
+</script>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
