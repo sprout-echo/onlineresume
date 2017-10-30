9e7101d0d61b847598f516ef6d52412568b7ce02
小YI酱 <775249762@qq.com>
2017-10-30 12:29:12 +0800

    Create main.vue

---
+++ src/pages/main.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="contain">
+        <ResEditor/>
+        <ResPreview/>
+    </div>
+</template>
+<script>
+import ResEditor from '../components/ResEditor'
+import ResPreview from '../components/ResPreview'
+
+export default{
+    name: 'main',
+    components:{ResEditor,ResPreview}
+}
+</script>
+<style>
+    body{
+        width:100%;
+        height:100%;
+        overflow:hidden;
+        margin:0;
+        padding:0;
+        background:#fff;
+    }
+    #contain{
+        width:100%;
+        height:100%;
+        overflow:auto;
+    }
+</style>
